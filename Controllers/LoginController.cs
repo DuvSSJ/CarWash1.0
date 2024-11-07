1487c6f4c71f62b5caa60f13106f555b86d87f2a
Tony2506 <anthonybbermudezloria@gmail.com>
2024-11-07 15:55:47 -0600

    log

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarWash1._0.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
