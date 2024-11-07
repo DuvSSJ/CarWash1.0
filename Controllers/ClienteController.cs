ef4e4303865a853bd61b766ecdfb976acdd083b3
Tony2506 <anthonybbermudezloria@gmail.com>
2024-11-07 15:45:29 -0600

    escritura

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -10,7 +10,7 @@ namespace CarWash1._0.Controllers
             var lstCliente = new List<ClienteModel>
         {
             new ClienteModel { IdCliente = 1, Nombre = "Juan", Apellido = "Pérez", Telefono ="61409659", Correo = "ABL@gmail.com",Direccion = " Perez zeledon, barrio la palma", Estado = true},
-            new ClienteModel { IdCliente = 2, Nombre = "kristes", Apellido = "fernandez", Telefono ="12369547", Correo = "kfj@gmail.com",Direccion = " Perez zeledon, barrio cooperativa", Estado = true},
+            new ClienteModel { IdCliente = 2, Nombre = "kristel", Apellido = "fernandez", Telefono ="12369547", Correo = "kfj@gmail.com",Direccion = " Perez zeledon, barrio cooperativa", Estado = true},
             new ClienteModel { IdCliente = 2, Nombre = "laura", Apellido = "jimenez", Telefono ="45256318", Correo = "peg@gmail.com",Direccion = " colombia, bogota", Estado = true},
         };
             return View(lstCliente);
